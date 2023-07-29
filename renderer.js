d9bb70cf08feeda4f407d22e915b1bb21fd5f54a
illictaffairs <syrnikov.school@gmail.com>
2023-07-29 09:00:25 +0200

    zoom/pan and pixel cursor added.

--- renderer.js
+++ renderer.js
@@ -62,7 +62,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvasRes = 1000;
   const PixelSize = canvas.width / canvasRes;
   let cooldownTime = 0;
+  let isCooldownActive = false;
   const cooldownInterval = 1000; // 1 second in milliseconds
+  let zoom = 1.0;
+  let panX = 0;
+  let panY = 0;
 
   // Function to initialize canvas from Firebase database
   function initializeCanvasFromDatabase() {
@@ -100,6 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function startCooldown() {
+    isCooldownActive = true; // Set the flag to indicate that the cooldown is active
     cooldownTime = 60 * 5; //cooldown
     updateCooldownDisplay();
     const cooldownIntervalId = setInterval(() => {
@@ -107,6 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
       updateCooldownDisplay();
       if (cooldownTime <= 0) {
         clearInterval(cooldownIntervalId);
+        isCooldownActive = false; // Reset the flag when the cooldown is over
       }
     }, cooldownInterval);
   }
@@ -174,6 +180,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Apply the inverse transformations (zoom and pan) to the canvas context
     ctx.save();
+
+    // Apply zoom transformation
+    ctx.scale(zoom, zoom);
+
+    // Apply pan transformation
+    ctx.translate(panX, panY);
+
     // Loop through the placedPixels array and redraw them with the correct transformations
     placedPixels.forEach((pixel) => {
       const StartX = pixel.x * PixelSize;
@@ -181,15 +194,34 @@ document.addEventListener("DOMContentLoaded", () => {
       ctx.fillStyle = pixel.color;
       ctx.fillRect(StartX, StartY, PixelSize, PixelSize);
     });
+
     // Restore the canvas context to its original state (remove the transformations)
+    if (
+      cursorX >= 0 &&
+      cursorX < canvas.width &&
+      cursorY >= 0 &&
+      cursorY < canvas.height
+    ) {
+      drawCursorBorder();
+    }
     ctx.restore();
   }
+
+  function handleCanvasMouseLeave() {
+    if (!isCooldownActive) {
+      // Reset the cursor position when the mouse leaves the canvas only if cooldown is not active
+      cursorX = -1;
+      cursorY = -1;
+      redraw();
+    }
+  }
+
   function handleCanvasMouseDown(e) {
     if (e.button === 0) {
       // Left mouse button for pixel placement
       const canvasBounds = canvas.getBoundingClientRect();
-      const x = e.clientX - canvasBounds.left;
-      const y = e.clientY - canvasBounds.top;
+      const x = (e.clientX - canvasBounds.left) / zoom - panX; // Apply zoom and pan
+      const y = (e.clientY - canvasBounds.top) / zoom - panY;
       const pixelX = Math.floor(x / PixelSize);
       const pixelY = Math.floor(y / PixelSize);
 
@@ -206,7 +238,102 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  let cursorX = 0;
+  let cursorY = 0;
+
+  function drawCursorBorder() {
+    if (!isCooldownActive) {
+      // Draw the border outline around the cursor position
+      const StartX = Math.floor(cursorX / PixelSize) * PixelSize;
+      const StartY = Math.floor(cursorY / PixelSize) * PixelSize;
+      ctx.strokeStyle = "#000"; // Set the border color (change as needed)
+      ctx.lineWidth = 2; // Set the border width (change as needed)
+      ctx.strokeRect(StartX, StartY, PixelSize, PixelSize);
+    }
+  }
+
+  function zoomIn(e) {
+    const cursorX = e.clientX - canvas.offsetLeft; // Cursor position relative to canvas
+    const cursorY = e.clientY - canvas.offsetTop;
+
+    // Calculate the change in canvas position due to zooming at the cursor center
+    const dx = (cursorX - panX) / zoom;
+    const dy = (cursorY - panY) / zoom;
+
+    // Apply the new zoom and pan offsets
+    zoom *= 1.05; // Increase zoom level by 5% (adjust this value for faster/slower zoom)
+    panX = cursorX - dx * zoom;
+    panY = cursorY - dy * zoom;
+    redraw();
+  }
+
+  function zoomOut(e) {
+    const cursorX = e.clientX - canvas.offsetLeft; // Cursor position relative to canvas
+    const cursorY = e.clientY - canvas.offsetTop;
+
+    // Calculate the change in canvas position due to zooming at the cursor center
+    const dx = (cursorX - panX) / zoom;
+    const dy = (cursorY - panY) / zoom;
+
+    // Apply the new zoom and pan offsets
+    zoom /= 1.05; // Decrease zoom level by 5% (adjust this value for faster/slower zoom)
+    panX = cursorX - dx * zoom;
+    panY = cursorY - dy * zoom;
+    redraw();
+  }
+
+  function pan(dx, dy) {
+    panX += dx; // Invert the pan direction here (use += instead of -=)
+    panY += dy; // Invert the pan direction here (use += instead of -=)
+    redraw();
+  }
+
+  canvas.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    if (e.deltaY < 0) {
+      zoomIn(e); // Pass the event object
+    } else {
+      zoomOut(e); // Pass the event object
+    }
+  });
+
+  let isDragging = false;
+  let lastX = 0;
+  let lastY = 0;
+
+  canvas.addEventListener("mousedown", (e) => {
+    if (e.button === 1 || e.button === 2) {
+      isDragging = true;
+      lastX = e.clientX;
+      lastY = e.clientY;
+    }
+  });
+
+  canvas.addEventListener("mouseup", () => {
+    isDragging = false;
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+      const dx = e.clientX - lastX;
+      const dy = e.clientY - lastY;
+      pan(dx, dy);
+      lastX = e.clientX;
+      lastY = e.clientY;
+    }
+  });
+
+  function handleCanvasMouseMove(e) {
+    // Update the cursor position on mouse move
+    const canvasBounds = canvas.getBoundingClientRect();
+    cursorX = (e.clientX - canvasBounds.left) / zoom - panX;
+    cursorY = (e.clientY - canvasBounds.top) / zoom - panY;
+    redraw();
+  }
+
   window.setColor = setColor;
+  canvas.addEventListener("mousemove", handleCanvasMouseMove);
+  canvas.addEventListener("mouseleave", handleCanvasMouseLeave);
   canvas.addEventListener("mousedown", handleCanvasMouseDown);
   canvas.addEventListener("contextmenu", (e) => e.preventDefault()); // Prevent right-click context menu
 
